527ea0f8566adc89006d3ce8b9d79822f762f4cc
Kamil-Szews <kamilszews.it@gmail.com>
2023-03-07 10:14:42 +0100

    First commit

---
+++ src/emailApp/EmailApp.java
@@ -0,0 +1,10 @@
+package emailApp;
+
+public class EmailApp {
+    public static void main(String[] args){
+        Email em1 = new Email("Kamil", "Szews");
+        Email em2 = new Email("Jan", "Kowalski");
+        Email em3 = new Email("Mateusz", "Nowak");
+        Email em4 = new Email("Emilia", "Szews");
+    }
+}
